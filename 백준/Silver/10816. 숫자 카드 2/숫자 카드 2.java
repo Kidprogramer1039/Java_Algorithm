b7eef0acdb263e424f5f4305c1e24058d0eb5e74
Kid Programmer <98392401+Kidprogramer1039@users.noreply.github.com>
2024-11-25 19:20:46 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1020 ms, Memory: 172692 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -2,43 +2,28 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
+        // 첫 번째 배열 크기와 데이터 입력
         int N = Integer.parseInt(bf.readLine());
         Map<Integer, Integer> hm = new HashMap<>();
-        String[] firstLine = bf.readLine().split(" "); // 한 줄 입력을 공백 기준으로 나눔
+        StringTokenizer st = new StringTokenizer(bf.readLine()); // StringTokenizer 사용
         for (int i = 0; i < N; i++) {
-            int val = Integer.parseInt(firstLine[i]);
-            boolean value = hm.containsKey(val);
-            if (value) {
-                hm.put(val, hm.get(val)+1);
-            }
-            else {
-                hm.put(val, 1);
-            }
+            int val = Integer.parseInt(st.nextToken());
+            hm.put(val, hm.getOrDefault(val, 0) + 1); // getOrDefault로 containsKey 제거
         }
 
-
-        int stat = N / 2;
-
-        // 두 번째 배열 크기 및 입력
+        // 두 번째 배열 크기와 데이터 입력
         int M = Integer.parseInt(bf.readLine());
-        int[] arr2 = new int[M];
-        String[] secondLine = bf.readLine().split(" "); // 한 줄 입력을 공백 기준으로 나눔
+        st = new StringTokenizer(bf.readLine());
         for (int i = 0; i < M; i++) {
-            arr2[i] = Integer.parseInt(secondLine[i]);
+            int item = Integer.parseInt(st.nextToken());
+            sb.append(hm.getOrDefault(item, 0)).append("\n"); // 결과를 StringBuilder에 추가
         }
 
-        for (int item : arr2) {
-            boolean res = hm.containsKey(item);
-            if (res) {
-                System.out.println(hm.get(item));
-            }
-            else {
-                System.out.println("0");
-            }
-        }
-
+        // 결과 출력
+        System.out.print(sb);
     }
 }
