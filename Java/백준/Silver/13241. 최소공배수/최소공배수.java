030ce5f538ebe947bd6dc878471364255324d72c
Kid Programmer <98392401+Kidprogramer1039@users.noreply.github.com>
2025-03-25 20:11:58 +0900

    [Silver V] Title: 최소공배수, Time: 108 ms, Memory: 14268 KB -BaekjoonHub

---
+++ Java/백준/Silver/13241. 최소공배수/최소공배수.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        
+        long gcd = getGCD(a, b);
+        long lcm = (a * b) / gcd;
+        
+        System.out.println(lcm);
+    }
+    
+    private static long getGCD(long x, long y) {
+        while (y != 0) {
+            long temp = y;
+            y = x % y;
+            x = temp;
+        }
+        return x;
+    }
+}
