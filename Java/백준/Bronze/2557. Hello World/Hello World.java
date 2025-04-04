bfa0d9474918873430c7e281b72a900e9ec923df
Coffee Hunter <98392401+Kidprogramer1039@users.noreply.github.com>
2025-04-04 21:07:32 +0900

    [Bronze V] Title: Hello World, Time: 96 ms, Memory: 13892 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World!");
+        }
+
+
+}
+
+
