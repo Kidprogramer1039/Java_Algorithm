1c217b152cbbebd8466cc31a355a7b5f3ceb03f6
Coffee Hunter <98392401+Kidprogramer1039@users.noreply.github.com>
2025-05-21 11:50:21 +0900

    [Bronze V] Title: 아스키 코드, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

---
+++ Java/백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringBuilder output = new StringBuilder(br.readLine());
+
+            System.out.println((int)output.charAt(0));
+
+        }
+
+
+}
+
+
